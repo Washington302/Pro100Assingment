529d55a055a6862631c81f0fd44e7d2d1c4ce135
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2021-08-05 19:04:33 -0600

    runs now

--- src/Pro100/Main.java
+++ src/Pro100/Main.java
@@ -3,12 +3,14 @@ package Pro100;
 public class Main {
 
     public static void main(String[] args) {
+        boolean cheese = false;
 	// write your code here
         //Hello partay people
         //hello
         //dis be on my branch?
-        while(1==1) {
+        while(cheese == false) {
             System.out.println("Cheese :)");
+            cheese = true;
         }
     }
 }
