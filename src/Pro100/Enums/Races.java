372627ee351391db4d5210038e6d4fb3776bfab9
fireball5559 <ohernadezpalacios@student.neumont.edu>
2021-08-08 13:15:06 -0600

    RACES ALMOST DONE

---
+++ src/Pro100/Enums/Races.java
@@ -0,0 +1,33 @@
+package Pro100.Enums;
+
+public enum Races {
+
+    BLACK_DRAGON,
+    BLUE_DRAGON,
+    BRASS_DRAGON,
+    BRONZE_DRAGON,
+    COPPER_DRAGON,
+    GOLD_DRAGON,
+    GREEN_DRAGON,
+    RED_DRAGON,
+    SILVER_DRAGON,
+    WHITE_DRAGON,
+
+    WOOD_ELVES,
+    HIGH_ELVES,
+    DARK_ELVES,
+
+    DEEP_GNOMES,
+    FOREST_GNOMES,
+    ROCK_GNOMES,
+
+    HALF_ELF,
+
+    LIGHTFOOT_HALFLING,
+    STOUT_HALFLING,
+
+
+
+
+}
+
