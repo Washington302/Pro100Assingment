e37dd3b29f355f4a28e3dcc66be7de4133a4676c
JimmyStephen <jstephen@student.neumont.edu>
2021-08-09 19:06:09 -0600

    Added Stats

--- src/Pro100/Main.java
+++ src/Pro100/Main.java
@@ -1,6 +1,8 @@
 package Pro100;
 
 import Pro100.Enums.Background;
+import Pro100.Enums.Classes;
+import Pro100.Enums.Races;
 import Pro100.Stats.Stats;
 
 import java.util.Locale;
@@ -16,10 +18,17 @@ public class Main {
 
         //testing that the background works
         Stats stats = new Stats();
+        Background background;
+        Races race;
+        Classes classes;
 
-        for(int i = 0; i < 10; i++) {
+        for(int i = 0; i < 100; i++) {
+            background = Background.randomizeBackground();
+            race = Races.randomizeRace();
+            classes = Classes.randomizeClass();
+            System.out.print("You are playing as a " + race + " " + classes + " you grew up as a " + background);
             int[] output = stats.getStatsRandom();
-            System.out.print("Your stats are: ");
+            System.out.print(", Your stats are: ");
             for(int l = 0; l <= 5; l++){
                 System.out.print(output[l]);
                 if(l != 5){
@@ -28,6 +37,6 @@ public class Main {
             }
             System.out.println();
         }
-        Menus.MainMenu();
+//        Menus.MainMenu();
     }
 }
