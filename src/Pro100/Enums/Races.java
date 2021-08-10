83ca0235062d62a1203b5241568adc434498c9df
JimmyStephen <jstephen@student.neumont.edu>
2021-08-10 15:46:53 -0600

    Changes to Races Implemented Stats Randomizer, and Stats based on Classes

--- src/Pro100/Enums/Races.java
+++ src/Pro100/Enums/Races.java
@@ -14,48 +14,66 @@ public enum Races {
     INFERNAL_TIEFLING;
 
 
-    public static Races randomizeRace(){
+    public static String randomizeRace(){
+        String retVal;
         int pick = new Random().nextInt(Races.values().length);
-        return Races.values()[pick];
+        Races race = Races.values()[pick];
+        switch(race){
+            case DRAGONBORN:
+                retVal = Dragonborn.randomizeDragonborn();
+                break;
+            case ELF:
+                retVal = Elf.randomizeElf();
+                break;
+            case GNOME:
+                retVal = Gnome.randomizeGnome();
+                break;
+            case HALFLING:
+                retVal =  Halfling.randomizeHafling();
+                break;
+            default:
+                retVal = Races.values()[pick].toString();
+        }
+        return retVal;
     }
 
     public enum Dragonborn{
-        BLACK_DRAGON,
-        BLUE_DRAGON,
-        BRASS_DRAGON,
-        BRONZE_DRAGON,
-        COPPER_DRAGON,
-        GOLD_DRAGON,
-        GREEN_DRAGON,
-        RED_DRAGON,
-        SILVER_DRAGON,
-        WHITE_DRAGON;
+        BLACK_DRAGONBORN,
+        BLUE_DRAGONBORN,
+        BRASS_DRAGONBORN,
+        BRONZE_DRAGONBORN,
+        COPPER_DRAGONBORN,
+        GOLD_DRAGONBORN,
+        GREEN_DRAGONBORN,
+        RED_DRAGONBORN,
+        SILVER_DRAGONBORN,
+        WHITE_DRAGONBORN;
 
-        public static Dragonborn randomizeClass(){
+        public static String randomizeDragonborn(){
             int pick = new Random().nextInt(Dragonborn.values().length);
-            return Dragonborn.values()[pick];
+            return Dragonborn.values()[pick].toString();
         }
     }
 
     public enum Elf{
-        WOOD_ELVES,
-        HIGH_ELVES,
-        DARK_ELVES;
+        WOOD_ELF,
+        HIGH_ELF,
+        DARK_ELF;
 
-        public static Elf randomizeClass(){
+        public static String randomizeElf(){
             int pick = new Random().nextInt(Elf.values().length);
-            return Elf.values()[pick];
+            return Elf.values()[pick].toString();
         }
     }
 
     public enum Gnome{
-        DEEP_GNOMES,
-        FOREST_GNOMES,
-        ROCK_GNOMES;
+        DEEP_GNOME,
+        FOREST_GNOME,
+        ROCK_GNOME;
 
-        public static Gnome randomizeClass(){
+        public static String randomizeGnome(){
             int pick = new Random().nextInt(Gnome.values().length);
-            return Gnome.values()[pick];
+            return Gnome.values()[pick].toString();
         }
     }
 
@@ -63,9 +81,9 @@ public enum Races {
         LIGHTFOOT_HALFLING,
         STOUT_HALFLING;
 
-        public static Halfling randomizeHafling(){
+        public static String randomizeHafling(){
             int pick = new Random().nextInt(Halfling.values().length);
-            return Halfling.values()[pick];
+            return Halfling.values()[pick].toString();
         }
     }
 }
--- src/Pro100/Main.java
+++ src/Pro100/Main.java
@@ -20,10 +20,20 @@ public class Main {
         //testing that the background works
         Stats stats = new Stats();
         Background background;
-        Races race;
+        String race;
         Classes classes;
         PlayerNames.firstName firstName;
         PlayerNames.lastName lastName;
+
+/*        for(int l = 0; l < 10; l++) {
+            classes = Classes.randomizeClass();
+            int[] stats1 = Stats.getStatsOrdered(classes);
+            System.out.print("You are a " + classes + " your stats are: ");
+            for (int i : stats1) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }*/
 
         for(int i = 0; i < 100; i++) {
             background = Background.randomizeBackground();
@@ -32,7 +42,7 @@ public class Main {
             firstName = PlayerNames.firstName.randomizeFirstName();
             lastName = PlayerNames.lastName.randomizeLastName();
             System.out.print("You are " + firstName + " " + lastName + ", the " + race + " " + classes + " you grew up as a " + background);
-            int[] output = stats.getStatsRandom();
+            int[] output = stats.getStatsOrdered(classes);
             System.out.print(", Your stats are: ");
             for(int l = 0; l <= 5; l++){
                 System.out.print(output[l]);
