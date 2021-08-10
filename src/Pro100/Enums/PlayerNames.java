6e5d0cd19c511dd1e37e93799df6a4a9d6b0855e
JoeMC0y <80136727+JoeMC0y@users.noreply.github.com>
2021-08-09 18:10:55 -0600

    Update PlayerNames.java

--- src/Pro100/Enums/PlayerNames.java
+++ src/Pro100/Enums/PlayerNames.java
@@ -3,14 +3,69 @@ package Pro100.Enums;
 public class PlayerNames {
 
     private enum firstName{
+        DREY,
+        DEWEY,
+        DANYON,
+        JETAIME,
+        WALT,
+        LAYLAND,
+        ACER,
+        TITUS,
+        ADONIS,
+        PIP,
+        LYNDON,
+        GRAY,
+        MIRAN,
+        DEONTAY,
+        KAVEN,
+        STOREY,
+        ZAREL,
+        TILO,
+        JETHRO,
+        DYLAN,
+        KALEB,
+        JAMIE,
+        ORLANDO,
+        JAMES
 
     }
 
     private enum middleName{
+        MAGNUS,
+        MALACHY,
+        MARLOW,
+        MASSIMO,
+        RAFFERTY,
+        ROSCOE,
+        SETH,
+        TEDY,
+        TEO,
+        THORIN,
+        URBAN,
+        WILDER,
+        OTIS,
+        TITUS,
+        MILLER,
+        ARAMIS,
+        KASEM,
+        GARIAN,
+        ATTICUS,
+        XEN,
+        KIT,
+        FRANK,
+        DANIEL,
+        JAMES,
+        HERNANDEZ,
+        JOSEPH
 
     }
 
     private enum lastName{
+        WINTERS,
+        WASHINGTON,
+        SPENCER,
+        PALACIOS,
+        STEPHEN
 
     }
 
