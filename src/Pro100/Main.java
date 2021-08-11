20536189a49f517f6d4ffa0433e2fb2305096702
JimmyStephen <jstephen@student.neumont.edu>
2021-08-10 18:15:21 -0600

    Added FileIO Class, and cleaned Main

--- src/Pro100/Main.java
+++ src/Pro100/Main.java
@@ -6,34 +6,18 @@ import Pro100.Enums.PlayerNames;
 import Pro100.Enums.Races;
 import Pro100.Stats.Stats;
 
-import java.util.Locale;
-
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         boolean cheese = true;
-        // write your code here
-        //Hello partay people
-        //hello
-        //dis be on my branch?
 
-        //testing that the background works
+        //testing
         Stats stats = new Stats();
         Background background;
         String race;
         Classes classes;
         PlayerNames.firstName firstName;
         PlayerNames.lastName lastName;
-
-/*        for(int l = 0; l < 10; l++) {
-            classes = Classes.randomizeClass();
-            int[] stats1 = Stats.getStatsOrdered(classes);
-            System.out.print("You are a " + classes + " your stats are: ");
-            for (int i : stats1) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-        }*/
 
         for(int i = 0; i < 100; i++) {
             background = Background.randomizeBackground();
