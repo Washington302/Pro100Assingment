85f2b978d145898792d5b09931a39127e97cb3ea
fireball5559 <ohernadezpalacios@student.neumont.edu>
2021-08-12 08:39:51 -0600

    Finished FileIO search for race Desc.

--- src/Pro100/FileIO.java
+++ src/Pro100/FileIO.java
@@ -5,15 +5,16 @@ import java.io.*;
 public class FileIO {
 
     public static void searchFile(String file) {
-        boolean isValid = true;
 
-        if (file.isEmpty() || file == null) {
+
+        if (file.isEmpty()) {
             System.out.println("Either String is null or empty");
-            isValid = false;
+
 
         } else {
             String a = file;
-            File filePath = new File("Pro100Assingment\\src\\Pro100\\DND_CHARACTERS\\" + a + ".txt");//this changes to current project and folder
+            File filePath = new File("src\\Pro100\\DND_CHARACTERS\\" + a + ".txt");//this changes to current project and folder
+
 
             if (!filePath.exists()) {
                 System.out.println("File does not exist, try again");
