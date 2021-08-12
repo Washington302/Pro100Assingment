7d304d6013f27e2a86128972ac8b3c1979a28aab
fireball5559 <ohernadezpalacios@student.neumont.edu>
2021-08-12 08:24:49 -0600

    FileIO stuff

--- src/Pro100/FileIO.java
+++ src/Pro100/FileIO.java
@@ -1,4 +1,43 @@
 package Pro100;
 
+import java.io.*;
+
 public class FileIO {
+
+    public static void searchFile(String file) {
+        boolean isValid = true;
+
+        if (file.isEmpty() || file == null) {
+            System.out.println("Either String is null or empty");
+            isValid = false;
+
+        } else {
+            String a = file;
+            File filePath = new File("DND_CHARACTERS\\" + a + ".txt");//this changes to current project and folder
+
+            if (!filePath.exists()) {
+                System.out.println("File does not exist, try again");
+                isValid = false;
+            } else {
+                BufferedReader br = null;
+                try {
+                    br = new BufferedReader(new FileReader(filePath));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                String st = null;
+                while (true) {
+                    try {
+                        if (!((st = br.readLine()) != null)) break;
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                    System.out.println(st);
+                }
+            }
+
+
+        }
+    }
 }
