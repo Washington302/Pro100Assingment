d04b8411ca0923715fa51ba76aa660f8bfde7c3d
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2021-08-13 17:29:04 -0600

    fixed menus

--- src/Pro100/CharacterGen.java
+++ src/Pro100/CharacterGen.java
@@ -5,10 +5,13 @@ import Pro100.Enums.Classes;
 import Pro100.Enums.PlayerNames;
 import Pro100.Enums.Races;
 import Pro100.Stats.Stats;
+import lib.ConsoleIO;
+
+import java.util.concurrent.TimeUnit;
 
 public class CharacterGen {
-    public static void RandomCharacter(){
-        FileIO file = new FileIO();
+    public static void RandomCharacter() throws InterruptedException {
+        //FileIO file = new FileIO();
         boolean cheese = true;
         //testing
         Stats stats = new Stats();
@@ -18,7 +21,9 @@ public class CharacterGen {
         PlayerNames.firstName firstName;
         PlayerNames.lastName lastName;
 
-        for(int i = 0; i < 100; i++) {
+        int maxCharacters = ConsoleIO.promptForInt("How many characters would you like to make?: ", 1, 999);
+
+        for(int i = 0; i < maxCharacters; i++) {
             background = Background.randomizeBackground();
             race = Races.randomizeRace();
             classes = Classes.randomizeClass();
@@ -26,15 +31,17 @@ public class CharacterGen {
             lastName = PlayerNames.lastName.randomizeLastName();
             System.out.print("You are " + firstName + " " + lastName + ", the " + race + " " + classes + " you grew up as a " + background);
             int[] output = stats.getStatsOrdered(classes);
-            System.out.print(", Your stats are: ");
+            System.out.print(", Your base stats are: \n\t");
             for(int l = 0; l <= 5; l++){
                 System.out.print(output[l]);
                 if(l != 5){
-                    System.out.print(", ");
+                    System.out.print(", \n\t");
                 }
             }
             System.out.println();
+            System.out.println();
         }
-        file.searchFileRace("Gnome");
+        TimeUnit.SECONDS.sleep(3);
+        //file.searchFileRace("Gnome");
     }
 }
