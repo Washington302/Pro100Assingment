e9dcb06b8799b0526e81e36021a04345e86d6344
DakotaSpencer <61034586+DakotaSpencer@users.noreply.github.com>
2021-08-12 18:29:04 -0600

    added CharacterGen class for neatness

---
+++ src/Pro100/CharacterGen.java
@@ -0,0 +1,40 @@
+package Pro100;
+
+import Pro100.Enums.Background;
+import Pro100.Enums.Classes;
+import Pro100.Enums.PlayerNames;
+import Pro100.Enums.Races;
+import Pro100.Stats.Stats;
+
+public class CharacterGen {
+    public static void RandomCharacter(){
+        FileIO file = new FileIO();
+        boolean cheese = true;
+        //testing
+        Stats stats = new Stats();
+        Background background;
+        String race;
+        Classes classes;
+        PlayerNames.firstName firstName;
+        PlayerNames.lastName lastName;
+
+        for(int i = 0; i < 100; i++) {
+            background = Background.randomizeBackground();
+            race = Races.randomizeRace();
+            classes = Classes.randomizeClass();
+            firstName = PlayerNames.firstName.randomizeFirstName();
+            lastName = PlayerNames.lastName.randomizeLastName();
+            System.out.print("You are " + firstName + " " + lastName + ", the " + race + " " + classes + " you grew up as a " + background);
+            int[] output = stats.getStatsOrdered(classes);
+            System.out.print(", Your stats are: ");
+            for(int l = 0; l <= 5; l++){
+                System.out.print(output[l]);
+                if(l != 5){
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
+        }
+        file.searchFileRace("Gnome");
+    }
+}
