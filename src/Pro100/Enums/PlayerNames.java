d1d1029b4c6bdc376753117c8be8f8d31543f2e0
JoeMC0y <80136727+JoeMC0y@users.noreply.github.com>
2021-08-16 18:34:07 -0600

    Update PlayerNames.java

--- src/Pro100/Enums/PlayerNames.java
+++ src/Pro100/Enums/PlayerNames.java
@@ -69,6 +69,37 @@ public class PlayerNames {
     }
 
     public enum middleName{
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+        I,
+        J,
+        K,
+        L,
+        M,
+        N,
+        O,
+        P,
+        Q,
+        R,
+        S,
+        T,
+        U,
+        V,
+        W,
+        X,
+        Y,
+        Z;
+
+        public static String randomizeMiddleName(){
+            int pick = new Random().nextInt(middleName.values().length);
+            return middleName.values()[pick].toString() + ".";
+        }
 
     }
 
